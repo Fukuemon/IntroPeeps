9376e92da672b9387844dc998987946bb567350b
Fukuemon <Fukuemony029@gmail.com>
2023-09-03 21:22:52 +0900

    ✨ マイプロフィール画面の作成 #2

--- src/app/components/Navigation/index.tsx
+++ src/app/components/Navigation/index.tsx
@@ -2,8 +2,30 @@
 
 import { Session } from "@supabase/auth-helpers-nextjs";
 import Link from "next/link";
+import { Database } from "../../../../utils/database.types";
+import useStore from "../../../store";
+import { useEffect } from "react";
+type ProfileType = Database["public"]["Tables"]["profiles"]["Row"];
 
-const Navigation = ({ session }: { session: Session | null }) => {
+const Navigation = ({
+  session,
+  profile,
+}: {
+  session: Session | null;
+  profile: ProfileType | null;
+}) => {
+  // プロフィールを更新
+  const { setUser } = useStore();
+
+  //storeに保存
+  useEffect(() => {
+    setUser({
+      id: session ? session.user.id : "",
+      email: session ? session.user.email! : "",
+      name: session && profile ? profile.name : "",
+      avatar_url: profile ? profile.avatar_url : "",
+    });
+  }, [session, profile, setUser]);
   return (
     <header className="border-b py-5 text-primary">
       <div className="container max-w-screen-xl mx-auto relative flex justify-center items-center ">
@@ -14,7 +36,7 @@ const Navigation = ({ session }: { session: Session | null }) => {
         <div className="absolute right-5">
           {session ? (
             <div className="flex space-x-4">
-              <Link href="/auth/profile">プロフィール</Link>
+              <Link href="/user/myprofile">プロフィール</Link>
             </div>
           ) : (
             <div>
