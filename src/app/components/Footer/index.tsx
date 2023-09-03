a599699ea44d1908e7bf569987e579c95c073e45
Fukuemon <Fukuemony029@gmail.com>
2023-09-03 12:54:14 +0900

    ✨ 共通で必要なレイアウトコンポーネント作成

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-100">
+      <div className="max-w-4xl w-full mx-auto h-24 flex items-center justify-center">
+        <div>@Fuku Blog</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
